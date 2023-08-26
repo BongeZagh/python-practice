df4f5517478bcc7e53cf124a16ed99e264a681db
BongeZagh <fullfilldestiny@gmail.com>
2023-08-26 17:48:35 +0800

    these is a interesting version finally fist line is the result I want but the rest still had some problem

--- data-cleaning-BPA/date.py
+++ data-cleaning-BPA/date.py
@@ -11,15 +11,16 @@ Title,Formatted Date,URL
 07-30-2009,[[Invalid Date]],https://www.brookspriceaction.com/viewtopic.php?t=13
 """
 
-# 使用正则表达式匹配标题中的日期并进行替换
+# 使用正则表达式匹配标题中的日期并进行转换
 pattern = r"(\d{2}-\d{2}-\d{4})"
-data = re.sub(pattern, lambda x: '[[' + datetime.strptime(x.group(), '%m-%d-%Y').strftime('%Y%m%d') + ']]', data)
+data = re.sub(pattern, lambda x: x.group(), data)  # 保持标题列的原始格式
 
 # 将转换后的日期替换到 Formatted Date 列
 data_lines = data.strip().split('\n')
 data_lines = [line.split(',') for line in data_lines]
 for line in data_lines[1:]:
-    line[1] = line[0]  # 将标题中的日期替换到 Formatted Date 列
+    title_date = datetime.strptime(line[0], '%m-%d-%Y').strftime('%Y%m%d')
+    line[1] = line[1].replace('[[invalid date]]', '[[' + title_date + ']]')
 
 # 重新组合数据行并打印结果
 data = '\n'.join([','.join(line) for line in data_lines])
